dd5faf97614cd3a928c278444e9a3d2fb96b73e3
Marko <msamoilovic@gmail.com>
2019-12-04 09:59:21 +0100

    promenjena boja CTA na login screenu

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -10,7 +10,7 @@ const Join = () => {
   return (
     <div className="joinContainerOutter">
       <div className="joinContainerInner">
-        <h1 className="header">Join the Chat Room</h1>
+        <h1 className="header">Dummy Chat App</h1>
         <div><input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} /></div>
         <div><input placeholder="Room" className="joinInput m20" type="text" onChange={(event) => setRoom(event.target.value)} /></div>
         <Link to={`/chat?name=${name}&room=${room}`} onClick={event => (!name || !room) ? event.preventDefault(): null}>
